f00357754241704c7329267462aa2b20340cd923
Anastasios Chondrogiannis <info@anastasioscho.gr>
2019-07-12 15:59:12 +0300

    feat: Set up the app bar
    Project: drawerlayoutnavigationview

--- drawerlayoutnavigationview/app/src/main/java/dev/anastasioscho/drawerlayoutnavigationview/MainActivity.kt
+++ drawerlayoutnavigationview/app/src/main/java/dev/anastasioscho/drawerlayoutnavigationview/MainActivity.kt
@@ -2,11 +2,21 @@ package dev.anastasioscho.drawerlayoutnavigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        setupAppBar()
+    }
+
+    private fun setupAppBar()
+    {
+        setSupportActionBar(toolbar)
     }
 }
