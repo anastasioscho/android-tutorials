1684a2ede0d10e6309920a029800790aafb2f67d
Anastasios Chondrogiannis <info@anastasioscho.gr>
2019-07-14 07:13:06 +0300

    feat: Set up the toggle button
    Project: drawerlayoutnavigationview

--- drawerlayoutnavigationview/app/src/main/java/dev/anastasioscho/drawerlayoutnavigationview/MainActivity.kt
+++ drawerlayoutnavigationview/app/src/main/java/dev/anastasioscho/drawerlayoutnavigationview/MainActivity.kt
@@ -1,11 +1,16 @@
 package dev.anastasioscho.drawerlayoutnavigationview
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -15,8 +20,36 @@ class MainActivity : AppCompatActivity()
         setupAppBar()
     }
 
+    override fun onPostCreate(savedInstanceState: Bundle?)
+    {
+        super.onPostCreate(savedInstanceState)
+
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?)
+    {
+        super.onConfigurationChanged(newConfig)
+
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean
+    {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun setupAppBar()
     {
         setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
     }
 }
