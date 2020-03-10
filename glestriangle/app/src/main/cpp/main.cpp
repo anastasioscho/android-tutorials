097aa1fc5cef30c7ddf725a18c61b32397beb0ad
Anastasios Chondrogiannis <info@anastasioscho.gr>
2020-03-10 13:22:43 +0200

    refactor: Only include the required GLM headers
    Project: glestriangle

--- glestriangle/app/src/main/cpp/main.cpp
+++ glestriangle/app/src/main/cpp/main.cpp
@@ -6,7 +6,8 @@
 #include <GLES3/gl31.h>
 #include <android/log.h>
 #include <malloc.h>
-#include "glm/glm.hpp"
+#include "glm/vec3.hpp"
+#include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
 
