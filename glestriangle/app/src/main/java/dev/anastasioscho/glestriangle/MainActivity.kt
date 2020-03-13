1c18e8e01c0eda8599a46a15d07e800612877d7e
Anastasios Chondrogiannis <info@anastasioscho.gr>
2020-03-13 12:20:55 +0200

    feat: Add and extract the texture image file
    Project: glestriangle

--- glestriangle/app/src/main/java/dev/anastasioscho/glestriangle/MainActivity.kt
+++ glestriangle/app/src/main/java/dev/anastasioscho/glestriangle/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.anastasioscho.glestriangle
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import java.io.File
 
 class MainActivity: AppCompatActivity() {
     private lateinit var customGLSurfaceView: CustomGLSurfaceView
@@ -12,5 +13,22 @@ class MainActivity: AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         customGLSurfaceView = findViewById(R.id.customGLSurfaceView)
+
+        extractFiles()
+    }
+
+    private fun extractFiles() {
+        val fileNames = arrayOf("texture-wood.jpg")
+        val filesDir = filesDir.absolutePath
+        val assetManager = assets
+
+        for (fileName in fileNames) {
+            val file = File(filesDir, fileName)
+
+            if (!file.exists()) {
+                val bytes = assetManager.open(fileName).readBytes()
+                file.writeBytes(bytes)
+            }
+        }
     }
 }
